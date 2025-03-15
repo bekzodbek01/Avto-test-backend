ce600f0a00ce1dacf428f8c4b9b754dd03c37adb
bekzodbek01 <bekzodbek5201@gmail.com>
2025-03-15 15:44:38 +0500

    111

--- config/settings.py
+++ config/settings.py
@@ -135,11 +135,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',  # JWT bilan login
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',  # Ro‘yxatdan o‘tish ochiq bo‘lishi uchun
+    ]
 }
 
 
